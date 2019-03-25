ba3fa9cc0845c3cc9d478d36e4f314359774adba
Emily Knowles <emilymnknowles@gmail.com>
2019-03-25 10:27:54 -0600

    added css to seedChoice

--- client-side/src/seedChoice.js
+++ client-side/src/seedChoice.js
@@ -6,24 +6,27 @@ import carrotCard from "./images/carrotCard.png"
 import tomatoCard from "./images/tomatoCard.png"
 
 class seedChoice extends React.Component {
+  componentDidMount(){
+    this.props.getPlant()
+  }
 
-      componentDidMount(){
-        this.props.getPlant()
-      }
   render(){
     console.log(this.props.plants)
-    const mappedPlants = this.props.plants.map(plant =>
-    <Link key={plant._id} to={`/seedChoice/${plant._id}`}>{plant.plantName}</Link>)
+    const mappedPlants = this.props.plants.map(plant => {
+      if (plant.plantName === 'Tomato'){
+        return <Link key={plant._id} to={`/seedChoice/${plant._id}`}><img className="cardSelections" src={tomatoCard} alt=""/></Link>
+      } else if (plant.plantName === 'Cabbage'){
+        return <Link key={plant._id} to={`/seedChoice/${plant._id}`}><img className="cardSelections" src={cabbageCard} alt=""/></Link>
+      } else if (plant.plantName === 'Carrot'){
+        return <Link key={plant._id} to={`/seedChoice/${plant._id}`}><img className="cardSelections" src={carrotCard} alt=""/></Link>
+      }
+    })
     return(
-      <div>
-        <p>Select which seed you want to plant</p>
-
-        {mappedPlants}
-
-        <img src={cabbageCard} alt=""/>
-        <img src={carrotCard} alt=""/>
-        <img src={tomatoCard} alt=""/>
-
+      <div className="seedChoiceContainer">
+        <p className="selectSeed">What would you like to Plant?</p>
+          <div className="seedSelection">
+            {mappedPlants}
+          </div>
       </div>
     );
   };
