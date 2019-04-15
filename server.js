a5f374ab58241df2b0e847443cc5bb44d34bcbb3
Veronica Giraldo <veronica9893@gmail.com>
2019-04-15 13:52:01 -0600

    updates for deploy

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require("path");
+const mongoose = require('mongoose');
+const app = express();
+const morgan = require('morgan')
+app.use(express.json());
+
+const PORT = process.env.PORT || 4204;
+// const port = 4204;
+
+app.use(morgan('dev'))
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.use('/plants', require('./routes/plantRoutes'))
+// app.use('/greenhouse', require('./routes/plantRoutes'))
+
+mongoose.connect(process.env.MONGODB_URI|| 'mongodb://localhost:27017/garden-game', {useNewUrlParser: true}, (err) => {
+    // if (err) console.error(err)
+
+    console.log('Connected to MongoDB!')
+})
+.catch(err => console.error(err))
+
+
+// Routes for Harvested Veggies
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`All set on port ${PORT}`)
+})
+
